09c8552b30367bbc77039575fa23a527b52600cb
LorisYanis <irvingyanis@gmail.com>
2023-04-03 15:59:11 +0200

    Html properties and _document.jsx was implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Top App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />;
+        </>
+    );
 }
