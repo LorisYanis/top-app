62554d521f5b79c7f805a300357522f7e3d2367b
LorisYanis <irvingyanis@gmail.com>
2023-04-03 17:38:54 +0200

    Added Htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,14 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
             <Head>
                 <title>Top App</title>
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"
+                />
             </Head>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </>
     );
 }
