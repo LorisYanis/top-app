04f2aae5ef67310acd18cf252db8242984e84c57
Yanis <irvingyanis@gmail.com>
2023-04-07 22:14:46 +0200

    added grid for layout and footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,28 @@
 import { FooterProps } from "./Footer.props";
 import cn from "classnames";
-import styles from "./Header.module.css";
+import { format } from "date-fns";
+import styles from "./Footer.module.css";
 
-export const Footer = ({ ...props }: FooterProps) => {
-    return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+    return (
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>
+                OwlTop © 2022 - {format(new Date(), "yyyy")} All rights reserved
+            </div>
+            <a className={styles.link} href="#" target="_blank">
+                Terms of use
+            </a>
+            <a className={styles.link} href="#" target="_blank">
+                Privacy Policy
+            </a>
+        </footer>
+
+        //<div className={cn(styles.text, styles["text-left"])}>
+        //     <span>OwlTop © 2023 All rights reserved</span>
+        //     </div>
+        //     <div className={cn(styles.text, styles["text-right"])}>
+        //         <span>Terms of Use</span>
+        //         <span>Privacy Policy</span>
+        //     </div>
+    );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,7 +17,7 @@ class MyDocument extends Document {
 
     render(): JSX.Element {
         return (
-            <Html lang="ru">
+            <Html lang="en">
                 <Head />
                 <body>
                     <Main />
