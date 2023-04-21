169acc5e0d3946d3746d8283606f0245b1862f4c
Yanis <irvingyanis@gmail.com>
2023-04-21 12:02:46 +0200

    added whileHover animation to button

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,9 +1,12 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
 export interface ButtonProps
-    extends DetailedHTMLProps<
-        ButtonHTMLAttributes<HTMLButtonElement>,
-        HTMLButtonElement
+    extends Omit<
+        DetailedHTMLProps<
+            ButtonHTMLAttributes<HTMLButtonElement>,
+            HTMLButtonElement
+        >,
+        "onAnimationStart" | "onDrag" | "onDragEnd" | "onDragStart" | "ref"
     > {
     appearance: "primary" | "ghost";
     arrow?: "right" | "down" | "none";
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,9 @@
+import { motion, useMotionValue } from "framer-motion";
 import { ButtonProps } from "./Button.props";
 import ArrowIcon from "./arrow.svg";
 import styles from "./Button.module.css";
 import cn from "classnames";
+import { useEffect } from "react";
 
 export const Button = ({
     appearance,
@@ -11,23 +13,22 @@ export const Button = ({
     ...props
 }: ButtonProps): JSX.Element => {
     return (
-        <button
+        <motion.button
             className={cn(styles.button, className, {
                 [styles.primary]: appearance === "primary",
                 [styles.ghost]: appearance === "ghost",
             })}
+            whileHover={{ scale: 1.04 }}
             {...props}
         >
             {children}
             {arrow !== "none" && (
                 <ArrowIcon
-                    className={
-                        cn(styles.arrow,     {
-                            [styles.arrowDown]: arrow === "down",
-                        })
-                    }
+                    className={cn(styles.arrow, {
+                        [styles.arrowDown]: arrow === "down",
+                    })}
                 />
             )}
-        </button>
+        </motion.button>
     );
 };
